518232641af4144e27e026d1d5123cdb91a6d30f
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-04-03 13:38:34 +0300

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
 }
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>js</li>
+                <li>html</li>
+                <li>css</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+};
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Home</a>
+            <a href="#">Home</a>
+        </div>
+    )
+}
+
 
 export default App;
